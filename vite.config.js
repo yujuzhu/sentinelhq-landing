0ee90a646a98cfc77f4ad2654d0fc41b40dc2410
yujuzhu <164347319+yujuzhu@users.noreply.github.com>
2025-12-06 13:01:15 +0100

    Finalize static i-Lab demo page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sentinelhq-landing/',   // ★ 这一行很关键：和你的仓库名一致
 })
